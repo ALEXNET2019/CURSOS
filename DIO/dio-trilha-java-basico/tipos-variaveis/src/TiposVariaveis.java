266d14d149599f75a2c81ea437748f97eb6de717
ALEXNET2019 <alex.belo.neto.2015@gmail.com>
2023-04-26 20:01:40 -0300

    Tipos e variaveis atualização

--- DIO/dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ DIO/dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,30 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Alex Neto";
+
+        System.out.println(meuNome);
+
+        double salarioMinimo = 2500;
+
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 2;
+
+        System.out.println(VALOR_DE_PI);
     }
 }
