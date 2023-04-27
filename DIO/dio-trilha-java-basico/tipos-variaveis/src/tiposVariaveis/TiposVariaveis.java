5af5204619296311e16480e51162adbadbca4842
ALEXNET2019 <alex.belo.neto.2015@gmail.com>
2023-04-26 21:27:08 -0300

    Organização de pastas

---
+++ DIO/dio-trilha-java-basico/tipos-variaveis/src/tiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,31 @@
+package tiposVariaveis;
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "Alex Neto";
+
+        System.out.println(meuNome);
+
+        double salarioMinimo = 2500;
+
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 2;
+
+        System.out.println(VALOR_DE_PI);
+    }
+}
